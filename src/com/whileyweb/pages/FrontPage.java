2a04f36708205d819096dedc93a61f0b167e37cd
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-10-14 17:36:03 +1300

    Fix for resizing problems.
    There were some problems with the code resizing widget.  Fixed now.

--- src/com/whileyweb/pages/FrontPage.java
+++ src/com/whileyweb/pages/FrontPage.java
@@ -2,9 +2,11 @@ package com.whileyweb.pages;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
@@ -17,84 +19,63 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.protocol.HttpContext;
 
+import com.whileyweb.util.HtmlPage;
+
 import jwebkit.http.HttpMethodDispatchHandler;
 
-public class FrontPage extends HttpMethodDispatchHandler {
+public class FrontPage extends HtmlPage {
 
-	public FrontPage() {
-		super(HttpMethodDispatchHandler.ALLOW_GET);
-	}
+	private static String[] examples = {
+			"Hello World","Absolute","IndexOf"
+	};
 
 	@Override
-	public void get(HttpRequest request, HttpResponse response, HttpContext context)
-			throws HttpException, IOException {
-		String uri = request.getRequestLine().getUri();
-		try {
-			List<NameValuePair> parameters = new URIBuilder(uri).getQueryParams();
-			ByteArrayOutputStream ous = new ByteArrayOutputStream();
-			PrintWriter writer = new PrintWriter(ous);
-			writePage(writer);
-			writer.flush();
-			response.setStatusCode(HttpStatus.SC_OK);
-			response.setEntity(new ByteArrayEntity(ous.toByteArray(), ContentType.TEXT_HTML));
-		} catch(URISyntaxException e) {
-			throw new HttpException("Invalid URI",e);
-		}
-	}
-
-	private void writePage(PrintWriter writer) {
-		writer.println("<!DOCTYPE html>");
-		writer.println("<html xmlns=\"http://www.w3.org/1999/xhtml\" dir=\"ltr\" lang=\"en-US\">");
-		writeHeader(writer);
-		writeBody(writer);
-		writer.println("</html>");
-	}
-
-	public void writeHeader(PrintWriter writer) {
-		writer.println("<head>");
-		writer.println("<title>Whiley Labs</title>");
-		writer.println("<link href=\"css/style.css\" rel=\"stylesheet\" type=\"text/css\">");
-	    writer.println("<script type=\"text/javascript\" src=\"js/jquery.js\"></script>");
-		writer.println("<script type=\"text/javascript\" src=\"js/jquery-ui.js\"></script>");
-		writer.println("<script type=\"text/javascript\" src=\"js/ace.js\" charset=\"utf-8\"></script>");
-		writer.println("<script type=\"text/javascript\" src=\"js/mode-whiley.js\"></script>");
-		writer.println("<script type=\"text/javascript\">var root_url = \"\"</script>");
-	    writer.println("<script type=\"text/javascript\" src=\"js/whileyweb.js\"></script>");
-	    writer.println("<script type=\"text/javascript\" src=\"js/wy.js\"></script>");
-	    writer.println("<script type=\"text/javascript\" src=\"js/wystd.js\"></script>");
-		writer.println("</head>");
-
-	}
-
-	public void writeBody(PrintWriter writer) {
-		writer.println("<body>");
-		writer.println("<div id='container'>");
-		writer.println("\t<div id='content'>");
-//		writer.println(
-//				"\t\t<p>From this page, you can run Whiley programs in your browser!  For more on Whiley, visit <a href='http://whiley.org'>whiley.org</a>.</p>");
-		writer.println("\t\t<div id='code'>// Write your Whiley program here</div>");
-		writer.println("\t\t\t<section id='toolbar'>");
-//		writer.println(
-//				"\t\t\t<button type='button' onClick='save();' title='Create a permalink of this program to share with others.'>Share</button>");
-		writer.println(
-				"\t\t\t<button type='button' onClick='compile();' title='Compile your program to check it is correct.'>Compile</button>");
-		writer.println(
-				"\t\t\t<button id = 'run' type='button' onClick='run();' title='Run your program to produce some output!'>Run</button>");
-		writer.println(
-				"\t\t\t<input id='verification' type='checkBox' checked='checked' title='Enable or disable compile-time verification'>Enable Verification</input>");
-		writer.println(
-				"\t\t\t<input id='counterexamples' type='checkBox' title='Enable or disable counterexample generation'>Generate Counterexamples</input>");
-		writer.println(
-				"\t\t\t<input id='showConsole' type='checkBox' onclick='showConsole(this.checked);' title='Show Console'>Show Console</input>");
-		writer.println(
-				"\t\t\t<input id='showJavaScript' type='checkBox' onclick='showJavaScript(this.checked);' title='Show generated JavaScript'>Show JavaScript</input>");
+	public void writeBodyContent(PrintStream writer, Map<String,String> parameters) {
+		writeCodeBlock(writer, parameters);
+		writeCommands(writer);
+		writeConfigOptions(writer);
 		writer.println("<img id='spinner' src='images/loading.gif' />");
-		writer.println("\t\t\t</section>");
+		writeExampleSelector(writer);
 		writer.println("\t\t<div id='messages'></div>");
 		writer.println("\t\t<textarea id='console' readonly='readonly'></textarea>");
 		writer.println("\t\t<textarea id='bin' readonly='readonly'></textarea>");
-		writer.println("\t\t</div>");
-		writer.println("\t</div>");
-		writer.println("</body>");
+	}
+
+	public void writeCodeBlock(PrintStream writer, Map<String, String> parameters) {
+		writer.print("<div id='code'></div>");
+	}
+
+	public void writeCommands(PrintStream writer) {
+		writer.println("<div id='cmdbar'>");
+		writer.println(
+				"<button type='button' onClick='compile();' title='Compile your program to check it is correct.'>Compile</button>");
+		writer.println(
+				"<button id = 'run' type='button' onClick='run();' title='Run your program to produce some output!'>Run</button>");
+		writer.println("</div>");
+	}
+
+	public void writeConfigOptions(PrintStream writer) {
+		writer.println("<div id='configbar'>");
+		writer.println(
+				"<input id='verification' type='checkBox' checked='checked' title='Enable or disable compile-time verification'>Verification</input>");
+		writer.println(
+				"<input id='showConsole' type='checkBox' onclick='showConsole(this.checked);' title='Show Console'>Console</input>");
+		writer.print("<br/>");
+		writer.println(
+				"<input id='counterexamples' type='checkBox' title='Enable or disable counterexample generation'>Counterexamples</input>");
+		writer.println(
+				"<input id='showJavaScript' type='checkBox' onclick='showJavaScript(this.checked);' title='Show generated JavaScript'>JavaScript</input>");
+		writer.println("</div>");
+	}
+
+	public void writeExampleSelector(PrintStream writer) {
+		writer.println("\t\t\t<div id='egbar'>");
+		writer.println("Examples:");
+		writer.println("<select name='eg'>");
+		for(int i=0;i!=examples.length;++i) {
+			writer.println("<option onclick='showExample(" + i + ");'>" + examples[i] + "</option>");
+		}
+		writer.println("</select>");
+		writer.println("</div>");
 	}
 }
